a32bcfd7742d0c46a9b20e6eeff3e5d0944e6cba
Abhinav Tanwar <tabhinav300@gmail.com>
2025-01-20 13:37:12 -0500

    changes

--- dist/index.js
+++ dist/index.js
@@ -21,7 +21,11 @@ const react_1 = require("./defaults/react");
 const cors_1 = __importDefault(require("cors"));
 const anthropic = new sdk_1.default();
 const app = (0, express_1.default)();
-app.use((0, cors_1.default)());
+app.use((0, cors_1.default)({
+    origin: 'https://lighthearted-pasca-e0594f.netlify.app', // Replace with your front-end URL
+    methods: 'GET,POST,PUT,DELETE',
+    credentials: true
+}));
 app.use(express_1.default.json());
 app.post("/template", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const prompt = req.body.prompt;
